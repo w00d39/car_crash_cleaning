257ed93c2f0c108c572311ac767d424097ce9bb9
apgodsen <apgodsen@iu.edu>
2024-11-27 16:22:05 -0600

    Update cleaning code.py

--- cleaning code.py
+++ cleaning code.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 #creates dataframe from csv file
 df = pd.read_csv("C:/Users/apgod/Downloads/cleaned_data_draft2.csv")
+#drop bicyclist values in person_type column
+df=df[df['PERSON_TYPE'] != 'Bicyclist']
 #drops rows with missing values in the columns VEHICLE_ID and EJECTION
 df.dropna(subset=["VEHICLE_ID", "EJECTION"], inplace=True)
+#drop useless columns
+df.drop(columns=['PED_ACTION'])
 #selects the first 400 rows of the dataframe
 df_subset = df.head(400)
 #saves the first 400 rows of the dataframe to a new csv file
