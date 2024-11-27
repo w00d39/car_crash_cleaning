39e6b44caa0a730f44b4555c410fc8ba51c3478c
apgodsen <apgodsen@iu.edu>
2024-11-27 16:11:47 -0600

    Update cleaning code.py

--- cleaning code.py
+++ cleaning code.py
@@ -5,6 +5,6 @@ df = pd.read_csv("C:/Users/apgod/Downloads/cleaned_data_draft2.csv")
 #drops rows with missing values in the columns VEHICLE_ID and EJECTION
 df.dropna(subset=["VEHICLE_ID", "EJECTION"], inplace=True)
 #selects the first 400 rows of the dataframe
-num_rows = 400
+df_subset = df.head(400)
 #saves the first 400 rows of the dataframe to a new csv file
-df.to_csv("cleaned_file.csv", index=False, nrows=num_rows)
+df_subset.to_csv("cleaned_file.csv", index=False)
